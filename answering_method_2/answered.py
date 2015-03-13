006e763f87ccc19e5269ca70ee0d3795f47b81a0
Aoeu <Aoeu@Aoeus-MacBook-Pro.local>
2015-03-13 02:27:36 -0400

    Fixed answering to use probabilities now

--- answering_method_2/answered.py
+++ answering_method_2/answered.py
@@ -3,24 +3,16 @@
 # First, input the filepath of the document, then a space.
 # Second, input the question.
 
-# ####### EXAMPLE QUESTIONS WITH OUTPUTS !! ##############
-
 # ex. python answered.py Pokemon.wiki The franchise celebrated its tenth anniversary in what year
-# 	The franchise celebrated Pokmon Tenth Anniversaryits tenth anniversary in 2006
 
-# What is the main staple of the Pokemon video game series
-# 	The main staple of the Pokmon video game series revolves around the catching and battling of Pokmon
+# TODO: Do more parsing. Split sentences better.
 
-# How many Pokemon manga series were released in Englis
-# There are various Pokmon manga series four of which were released in English by Viz Media and seven of them released in English by Chuang Yi
 
-# Which generation of games does Pokemon X and Y for the Nintendo 3DS belong to
-# 	Officially announced on January 8 2013 and released simultaneously worldwide on October 12 2013 Pokmon X and Y for the Nintendo 3DS are part of the sixth generation of games
-
-# TODO: Make it use the unigram/bigram PROBABILITIES instead of just weighting everything equally.
-
+from __future__ import division
 import sys
 import re
+import nltk.data
+import math
 
 filePath = sys.argv[1]
 
@@ -31,47 +23,90 @@ for i in range(2, len(sys.argv)):
 
 questionText = str.split(question)
 
-unigram_weight = 1
-bigram_weight = 2
-trigram_weight = 3
-
 file = open(filePath, "rw+")
-lines = file.readlines()
+data = file.read()
 
-bestScore = -1
+data = re.sub('\n', '\n ',data)
+data = re.sub('\[.*\]', '', data)
+data = re.sub('[^A-Za-z0-9.?!,\n ]+', '', data)
+
+unigrams = {}
+unigramTotal = 0
+
+bigrams = {}
+bigramTotal = 0
+
+trigrams = {}
+trigramTotal = 0
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+sentences = tokenizer.tokenize(data)
+
+for sentence in sentences:
+	words = sentence.split()
+	for i in range(0, len(words) - 1) :
+		if words[i] in unigrams:
+			unigrams[words[i]] = unigrams[words[i]] + 1
+		else:
+			unigrams[words[i]] = 1
+		unigramTotal+=1
+
+		if i > 0:
+			x = words[i-1] + " " + words[i];
+			if x in bigrams:
+				bigrams[x] = bigrams[x] + 1
+			else:
+				bigrams[x] = 1
+			bigramTotal+=1
+
+		if i > 1:
+			x = words[i-2] + " " + words[i-1] + " " + words[i];
+			if x in trigrams:
+				trigrams[x] = trigrams[x] + 1
+			else:
+				trigrams[x] = 1
+			trigramTotal+=1
+
+for key in unigrams:
+	unigrams[key] = math.log(unigrams[key] / unigramTotal)
+
+for key in bigrams:
+	bigrams[key] = math.log(bigrams[key] / bigramTotal)
+
+for key in trigrams:
+	trigrams[key] = math.log(trigrams[key] / trigramTotal)
+
+
+bestScore = 1
 
 bestSentence = ""
 
-for line in lines:
-	# Naively assume all periods are the end of a sentence. Every all sentence is ended by a period. "Mr. Jones disagrees!"
-	line = re.sub('[^A-Za-z0-9. ]+', '', line)
-	sentences = line.split(".")
-	for sentence in sentences:
-		score = 0;
+for sentence in sentences:
+	score = 0;
 
-		words = sentence.split();
-#		print(sentence)
-		# UNIGRAM
-		for word in words:
-			if word in questionText:
-				score += unigram_weight
-				
+	words = sentence.split();
 
-		# BIGRAM
-		for i in range(0, len(words) - 1):
-			bigram = words[i] + " " + words[i + 1]
-			if bigram in question:
-				score += bigram_weight
+	# UNIGRAM
+	for word in words:
+		if word in questionText:
+			score += unigrams[word]
+			
+	# BIGRAM
+	for i in range(0, len(words) - 1):
+		bigram = words[i] + " " + words[i + 1]
+		if bigram in question:
+			score += 2 * bigrams[bigram]
 
-		# TRIGRAM
-		for i in range(0, len(words) - 2):
-			trigram = words[i] + " " + words[i + 1] + " " + words[i + 2]
-			if trigram in question:
-				score += trigram_weight
+	# TRIGRAM
+	for i in range(0, len(words) - 2):
+		trigram = words[i] + " " + words[i + 1] + " " + words[i + 2]
+		if trigram in question:
+			score += 3 * trigrams[trigram]
 
-		if score > bestScore:
-			bestScore = score
-			bestSentence = sentence
+	if score < bestScore:
+		bestScore = score
+		bestSentence = sentence
 
 
 print(bestSentence)
